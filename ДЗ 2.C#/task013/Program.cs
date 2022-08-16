cd91016cd8a49b263ea4541298b41e4b6017bf51
ilyanik77 <ilyakhlamov77@gmail.com>
2022-08-16 14:54:41 +0300

    first commit

---
+++ ДЗ 2.C#/task013/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число от 0 до 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) {
+    Console.WriteLine("Третьей цифры нет");
+} else {
+    int result = number % 10;
+    Console.WriteLine(result);
+}
---
+++ ДЗ 2.C#/task015/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7) {
+    Console.WriteLine("День выходной!"); 
+} else {
+    Console.WriteLine("День рабочий!");
+}
